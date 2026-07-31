611b97583391f844418584457b9affb76133fbac
Zebax <sebasgamer8538@gmail.com>
2026-07-31 09:20:59 -0500

    Add files via upload
    Added 2 new files for the project

---
+++ Logica_Hipoteca_Inversa.py
@@ -0,0 +1,7 @@
+# Aca se encuentra la logica de el proyecto "Calculadora de Hipoteca Inversa"
+
+def desembolso_mensual():
+    """El proposito de esta funcion, es calcular el desembolso mensual por parte
+    de el banco a la persono que esta solicitando dicho servicio recibiendo
+    los datos tales como el plazo, """
+    return
---
+++ Tests_Hipoteca_Inversa.py
@@ -0,0 +1,5 @@
+import unittest
+import Logica_Hipoteca_Inversa
+
+class Tests_HipotecaInversaLogica( unittest.TestCase ):
+    ...

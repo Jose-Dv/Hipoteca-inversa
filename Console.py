745b3054fb6f738ad99150781409565e0a19cdfc
Zebax <sebasgamer8538@gmail.com>
2026-08-11 20:50:47 -0500

    Update Console.py
    se agrego el correcto sistema por consola

--- Console.py
+++ Console.py
@@ -1,10 +1,18 @@
 # consola de el proyecto de hipoteca inversa
 
 import Logica_Hipoteca_Inversa
+
 print("Este programa permite calcular la hipoteca inversa, en base a: \n 1.Valor Propiedad \n 2.Porcentaje de desembolso \n 3.Tasa de interes \n 4.Plazo en meses ")
+
 valor = float(input("Valor Propiedad: "))
-desembolso = float(input("Porcentaje de desembolso: "))
+desembolso = float(input("Porcentaje de desembolso: ")) / 100
 tasa = float(input("Tasa de interes: ")) / 100
 plazo = int(input("Plazo en meses: "))
 
-print(Logica_Hipoteca_Inversa.desembolso_mensual(valor,desembolso,tasa,plazo))
+try:
+    cuota, abonos, intereses = Logica_Hipoteca_Inversa.desembolso_mensual(valor, desembolso, tasa, plazo)
+    print("Cuota mensual:", cuota)
+    print("Total abonos:", abonos)
+    print("Total intereses:", intereses)
+except Logica_Hipoteca_Inversa.HipotecaInversaError as error:
+    print(error)
